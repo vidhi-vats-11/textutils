efb90c446fa0b6fd56ab570b40280baf70743690
vidhi vats <vatsvidhisharma@gmail.com>
2023-01-11 20:40:25 +0530

    textutils, first commit

--- textutils/views.py
+++ textutils/views.py
@@ -1,4 +1,4 @@
-# I have created this file - Vidhi
+# I have created this file - vidhi vats
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -6,18 +6,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-    # return HttpResponse("Home")
-
-
-def ex1(request):
-    sites = ['''<h1>For Entertainment  </h1> <a href="https://www.youtube.com/"> Youtube Videos</a> ''',
-             '''<h1>For Interaction  </h1> <a href="https://www.facebook.com/"> Facebook</a> ''',
-             '''<h1>For Insight  </h1> <a href="https://www.ted.com/talks"> Ted Talks</a> ''',
-             '''<h1>For Internship  </h1> <a href="https://www.internshala.com">Internship</a> ''']
-    return HttpResponse((sites))
 
 def analyze(request):
-    #Get the text
+    # Get the text
     djtext = request.POST.get('text', 'default')
 
     # Check checkbox values
@@ -25,45 +16,66 @@ def analyze(request):
     fullcaps = request.POST.get('fullcaps', 'off')
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    numberremover = request.POST.get('numberremover', 'off')
 
-    #Check which checkbox is on
+    # Check which checkbox is on
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
-        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
 
-    elif(fullcaps=="on"):
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (fullcaps == "on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
 
         params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
 
-    elif(extraspaceremover=="on"):
+    if (extraspaceremover == "on"):
         analyzed = ""
         for index, char in enumerate(djtext):
-            if not(djtext[index] == " " and djtext[index+1]==" "):
+            # It is for if a extraspace is in the last of the string
+            if char == djtext[-1]:
+                if not (djtext[index] == " "):
+                    analyzed = analyzed + char
+
+            elif not (djtext[index] == " " and djtext[index + 1] == " "):
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
 
-    elif (newlineremover == "on"):
+    if (newlineremover == "on"):
         analyzed = ""
         for char in djtext:
-            if char != "\n" and char!="\r":
+            if char != "\n" and char != "\r":
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error")
 
+    if (numberremover == "on"):
+        analyzed = ""
+        numbers = '0123456789'
+
+        for char in djtext:
+            if char not in numbers:
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (
+            removepunc != "on" and newlineremover != "on" and extraspaceremover != "on" and fullcaps != "on" and numberremover != "on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
+
+
+def about(request):
+    return render(request, 'about.html')
